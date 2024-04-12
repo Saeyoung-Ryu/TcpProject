cd11e65b30993f8122e223022ac1dd58d6cf4906
유세영 <usaeyoung9514@gmail.com>
2024-04-12 18:56:02 +0900

    SQL File 수정

--- HttpServer/MySql/ProjectDB.sql
+++ HttpServer/MySql/ProjectDB.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80033
  File Encoding         : 65001
 
- Date: 16/02/2024 18:43:43
+ Date: 12/04/2024 18:53:56
 */
 
 SET NAMES utf8mb4;
@@ -23,17 +23,26 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `tblMatchHistory`;
 CREATE TABLE `tblMatchHistory` (
   `seq` bigint NOT NULL AUTO_INCREMENT,
-  `playerSeq` bigint NOT NULL,
+  `suid` bigint NOT NULL,
   `data` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`seq`),
-  UNIQUE KEY `idxPlayerSeq` (`playerSeq`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  UNIQUE KEY `idxPlayerSuid` (`suid`)
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- ----------------------------
 -- Records of tblMatchHistory
 -- ----------------------------
 BEGIN;
-INSERT INTO `tblMatchHistory` VALUES (6, 1, '{\"1\":{\"1\":\"amy\",\"2\":\"samy\"},\"2\":{\"1\":\"amy2\",\"2\":\"samy\"},\"3\":{\"1\":\"amy4\",\"2\":\"samy\"}}');
+INSERT INTO `tblMatchHistory` VALUES (6, 1, '[{\"Win\":\"amy\",\"Lose\":\"samy\"},{\"Win\":\"amy2\",\"Lose\":\"samy\"},{\"Win\":\"amy4\",\"Lose\":\"samy\"},{\"Win\":\"NoNo\",\"Lose\":\"Sy1234\"},{\"Win\":\"NoNo\",\"Lose\":\"Sy1234\"},{\"Win\":\"Amy123\",\"Lose\":\"Sy1234\"}]');
+INSERT INTO `tblMatchHistory` VALUES (7, 2, '[{\"Win\":\"NoNo\",\"Lose\":\"Sy1234\"},{\"Win\":\"NoNo\",\"Lose\":\"Sy1234\"}]');
+INSERT INTO `tblMatchHistory` VALUES (11, 5, '[{\"Win\":\"SAMY\",\"Lose\":\"Amy123\"}]');
+INSERT INTO `tblMatchHistory` VALUES (12, 4, '[{\"Win\":\"SAMY\",\"Lose\":\"Amy123\"},{\"Win\":\"Test12\",\"Lose\":\"Amy123\"},{\"Win\":\"Amy123\",\"Lose\":\"Sy1234\"}]');
+INSERT INTO `tblMatchHistory` VALUES (13, 9, '[{\"Win\":\"Test12\",\"Lose\":\"Test\"},{\"Win\":\"Test12\",\"Lose\":\"Amy123\"}]');
+INSERT INTO `tblMatchHistory` VALUES (14, 8, '[{\"Win\":\"Test12\",\"Lose\":\"Test\"}]');
+INSERT INTO `tblMatchHistory` VALUES (17, 11, '[{\"Win\":\"Test1111\",\"Lose\":\"Test2222\"},{\"Win\":\"Test1111\",\"Lose\":\"Test2222\"},{\"Win\":\"Test1111\",\"Lose\":\"Test2222\"},{\"Win\":\"Test1111\",\"Lose\":\"Test2222\"},{\"Win\":\"Test1111\",\"Lose\":\"Test2222\"},{\"Win\":\"Test1111\",\"Lose\":\"Test2222\"}]');
+INSERT INTO `tblMatchHistory` VALUES (18, 12, '[{\"Win\":\"Test1111\",\"Lose\":\"Test2222\"},{\"Win\":\"Test1111\",\"Lose\":\"Test2222\"},{\"Win\":\"Test1111\",\"Lose\":\"Test2222\"},{\"Win\":\"Test1111\",\"Lose\":\"Test2222\"},{\"Win\":\"Test1111\",\"Lose\":\"Test2222\"},{\"Win\":\"Test1111\",\"Lose\":\"Test2222\"}]');
+INSERT INTO `tblMatchHistory` VALUES (31, 7184487515594686481, '[{\"Win\":\"TestUser2\",\"Lose\":\"TestUser1\"}]');
+INSERT INTO `tblMatchHistory` VALUES (32, 7184487329157873680, '[{\"Win\":\"TestUser2\",\"Lose\":\"TestUser1\"}]');
 COMMIT;
 
 -- ----------------------------
@@ -42,23 +51,21 @@ COMMIT;
 DROP TABLE IF EXISTS `tblPlayer`;
 CREATE TABLE `tblPlayer` (
   `seq` bigint NOT NULL AUTO_INCREMENT,
+  `suid` bigint NOT NULL,
+  `accountId` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `loginType` int NOT NULL,
   `nickName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `createTime` datetime NOT NULL,
   PRIMARY KEY (`seq`),
-  UNIQUE KEY `idxSeq` (`seq`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  UNIQUE KEY `idxSuid` (`suid`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- ----------------------------
 -- Records of tblPlayer
 -- ----------------------------
 BEGIN;
-INSERT INTO `tblPlayer` VALUES (1, 'Sy1234', '2024-02-13 05:40:41');
-INSERT INTO `tblPlayer` VALUES (2, 'Amy', '2024-02-13 06:56:33');
-INSERT INTO `tblPlayer` VALUES (3, 'Sy12345', '2024-02-13 06:56:38');
-INSERT INTO `tblPlayer` VALUES (4, 'Amy123', '2024-02-13 06:56:33');
-INSERT INTO `tblPlayer` VALUES (5, 'SAMY', '2024-02-16 07:39:51');
-INSERT INTO `tblPlayer` VALUES (6, 'SAMY123', '2024-02-16 07:44:33');
-INSERT INTO `tblPlayer` VALUES (7, 'SAMY1234', '2024-02-16 07:52:53');
+INSERT INTO `tblPlayer` VALUES (19, 7184488619447418898, '', 1, 'RRRRR', '2024-04-12 09:52:48');
+INSERT INTO `tblPlayer` VALUES (20, 7184488634827931667, '103886615978569164912', 2, 'usaeyoung9514', '2024-04-12 09:52:52');
 COMMIT;
 
 -- ----------------------------
@@ -67,18 +74,28 @@ COMMIT;
 DROP TABLE IF EXISTS `tblRank`;
 CREATE TABLE `tblRank` (
   `seq` bigint NOT NULL AUTO_INCREMENT,
-  `playerSeq` bigint NOT NULL,
+  `suid` bigint NOT NULL,
   `winCount` int NOT NULL,
   `loseCount` int NOT NULL,
   `point` int NOT NULL,
   PRIMARY KEY (`seq`),
-  UNIQUE KEY `idxPlayerSeq` (`playerSeq`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  UNIQUE KEY `idxPlayerSuid` (`suid`)
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- ----------------------------
 -- Records of tblRank
 -- ----------------------------
 BEGIN;
+INSERT INTO `tblRank` VALUES (6, 2, 2, 0, 110);
+INSERT INTO `tblRank` VALUES (7, 1, 4, 3, 108);
+INSERT INTO `tblRank` VALUES (10, 5, 1, 0, 105);
+INSERT INTO `tblRank` VALUES (11, 4, 1, 2, 97);
+INSERT INTO `tblRank` VALUES (12, 9, 2, 0, 110);
+INSERT INTO `tblRank` VALUES (13, 8, 0, 1, 96);
+INSERT INTO `tblRank` VALUES (17, 11, 6, 0, 130);
+INSERT INTO `tblRank` VALUES (18, 12, 0, 6, 76);
+INSERT INTO `tblRank` VALUES (31, 7184487515594686481, 1, 0, 105);
+INSERT INTO `tblRank` VALUES (32, 7184487329157873680, 0, 1, 96);
 COMMIT;
 
 -- ----------------------------
@@ -107,11 +124,25 @@ COMMIT;
 -- ----------------------------
 DROP PROCEDURE IF EXISTS `spGetMatchHistory`;
 delimiter ;;
-CREATE PROCEDURE `spGetMatchHistory`(IN _playerSeq bigint)
+CREATE PROCEDURE `spGetMatchHistory`(IN _suid bigint)
 BEGIN
     SELECT *
     FROM tblMatchHistory
-    WHERE playerSeq = _playerSeq limit 1;
+    WHERE suid = _suid limit 1;
+END
+;;
+delimiter ;
+
+-- ----------------------------
+-- Procedure structure for spGetPlayerWithAccountId
+-- ----------------------------
+DROP PROCEDURE IF EXISTS `spGetPlayerWithAccountId`;
+delimiter ;;
+CREATE PROCEDURE `spGetPlayerWithAccountId`(IN _accountId varchar(255), IN _loginType int)
+BEGIN
+    SELECT *
+    FROM tblPlayer
+    WHERE `accountId` = _accountId and `loginType` = _loginType limit 1;
 END
 ;;
 delimiter ;
@@ -145,15 +176,29 @@ END
 delimiter ;
 
 -- ----------------------------
+-- Procedure structure for spGetPlayerWithSuid
+-- ----------------------------
+DROP PROCEDURE IF EXISTS `spGetPlayerWithSuid`;
+delimiter ;;
+CREATE PROCEDURE `spGetPlayerWithSuid`(IN _suid bigint)
+BEGIN
+    SELECT *
+    FROM tblPlayer
+    WHERE suid = _suid limit 1;
+END
+;;
+delimiter ;
+
+-- ----------------------------
 -- Procedure structure for spGetRank
 -- ----------------------------
 DROP PROCEDURE IF EXISTS `spGetRank`;
 delimiter ;;
-CREATE PROCEDURE `spGetRank`(IN _playerSeq bigint)
+CREATE PROCEDURE `spGetRank`(IN _suid bigint)
 BEGIN
     SELECT *
     FROM tblRank
-    WHERE playerSeq = _playerSeq limit 1;
+    WHERE suid = _suid limit 1;
 END
 ;;
 delimiter ;
@@ -189,10 +234,10 @@ delimiter ;
 -- ----------------------------
 DROP PROCEDURE IF EXISTS `spSetMatchHistory`;
 delimiter ;;
-CREATE PROCEDURE `spSetMatchHistory`(IN _playerSeq bigint, IN _data varchar(255))
+CREATE PROCEDURE `spSetMatchHistory`(IN _suid bigint, IN _data varchar(255))
 BEGIN
-    INSERT INTO tblMatchHistory (playerSeq, data)
-    VALUES (_playerSeq, _data)
+    INSERT INTO tblMatchHistory (suid, data)
+    VALUES (_suid, _data)
     on duplicate key update
         `data` = _data;
 END
@@ -204,12 +249,15 @@ delimiter ;
 -- ----------------------------
 DROP PROCEDURE IF EXISTS `spSetPlayer`;
 delimiter ;;
-CREATE PROCEDURE `spSetPlayer`(IN _seq bigint, IN _nickname varchar(255), IN _createTime datetime)
+CREATE PROCEDURE `spSetPlayer`(IN _suid bigint, IN _accountId varchar(255), IN _loginType int,
+                                                   IN _nickname varchar(255), IN _createTime datetime)
 BEGIN
-    INSERT INTO tblPlayer (seq, nickName, createTime)
-    VALUES (_seq, _nickname, _createTime)
+    INSERT INTO tblPlayer (suid, accountId, loginType, nickName, createTime)
+    VALUES (_suid, _accountId, _loginType, _nickname, _createTime)
     on duplicate key update
-                         `seq` = _seq,
+                         `suid` = _suid,
+                         `accountId` = _accountId,
+                         `loginType` = _loginType,
                          `nickName` = _nickname,
                          `createTime` = _createTime;
 END
@@ -221,10 +269,10 @@ delimiter ;
 -- ----------------------------
 DROP PROCEDURE IF EXISTS `spSetRank`;
 delimiter ;;
-CREATE PROCEDURE `spSetRank`(IN _playerSeq bigint, IN _winCount int, IN _loseCount int, IN _point int)
+CREATE PROCEDURE `spSetRank`(IN _suid bigint, IN _winCount int, IN _loseCount int, IN _point int)
 BEGIN
-    INSERT INTO tblRank (playerSeq, winCount, loseCount, point)
-    VALUES (_playerSeq, _winCount, _loseCount, _point)
+    INSERT INTO tblRank (suid, winCount, loseCount, point)
+    VALUES (_suid, _winCount, _loseCount, _point)
     on duplicate key update
                          `winCount` = _winCount,
                          `loseCount` = _loseCount,
